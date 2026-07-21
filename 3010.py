70c85302449e490dced32832088281689ad49069
Tun6970065IT <69070065@kmitl.ac.th>
2026-07-21 18:30:34 +0700

    add2

--- 3010.py
+++ 3010.py
@@ -5,18 +5,18 @@ def main():
     x = int(input())
     y = int(input())
     if not x  and not y :
-        print("0")
-    elif y == 0:
+        print("O")
+    elif not y:
         print("X")
-    elif x == 0:
+    elif not x:
         print("Y")
-    elif x > 0  and y > 0 :
+    elif x > 0  and y > 0:
         print("Q1")
-    elif x < 0 and y > 0 :
+    elif x < 0 < y:
         print("Q2")
-    elif x < 0 and y < 0 :
+    elif x < 0 and y < 0:
         print("Q3")
-    elif x > 0  and y < 0 :
+    else:
         print("Q4")
 
 main()
