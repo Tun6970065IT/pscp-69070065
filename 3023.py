f459efcdad99ae55f5b73c3c0a17d5f535c11373
Tun6970065IT <69070065@kmitl.ac.th>
2026-08-05 12:40:36 +0700

    Update 3023.py

--- 3023.py
+++ 3023.py
@@ -1,7 +1,12 @@
 """calculator"""
 
 n = int(input())
+
 if n == 1:
-    print("1")
+    print(1)
 else:
-    print(n * 2)
+    count = 0
+    for i in range(1, n + 1):
+        count += len(str(i))
+    total_press = count + n
+    print(total_press)
