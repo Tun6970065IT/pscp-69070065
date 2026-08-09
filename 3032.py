c2dae076c7edea2049681c303e325ebf2684e213
Tun6970065IT <69070065@kmitl.ac.th>
2026-08-09 18:39:01 +0700

    Update 3032.py

--- 3032.py
+++ 3032.py
@@ -1,5 +1,15 @@
 """test score"""
 
-my_list = [1 ,  2 , 3]
-max_num = max(my_list)
-print(max_num)
+test_score = int(input())
+max_score = -1
+count = 0
+for i in range(test_score):
+    score = int(input())
+    if score > max_score:
+        max_score = score
+        count = 1
+    elif score == max_score:
+        count += 1
+    i += 1
+print(max_score)
+print(count)
