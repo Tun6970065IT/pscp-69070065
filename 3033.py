487850a7a60306f85d912a8c770921914a080c26
Tun6970065IT <69070065@kmitl.ac.th>
2026-08-09 22:27:02 +0700

    Update 3033.py

--- 3033.py
+++ 3033.py
@@ -1,7 +1,10 @@
 """กระดาษห่อของขวัญ"""
 
 paper = input()
-paper_split = paper.split
+paper_split = paper.split(" ")
 r = float(paper_split[0])
-high = float(paper_split[1])
-distance = float(paper_split[2])
+h = float(paper_split[1])
+s = float(paper_split[2])
+wide = h + (r * 2)
+long = (2 * 3.14 * r) + s
+print(f"{wide:.2f} {long:.2f}")
