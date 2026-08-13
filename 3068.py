28d3a0cd69495cf0129296b9adbbd6c09b002432
Tun6970065IT <69070065@kmitl.ac.th>
2026-08-13 11:09:11 +0700

    Update 3068.py

--- 3068.py
+++ 3068.py
@@ -1,14 +1,13 @@
-"""ปีอธิกสุรทิน"""
+"""leapyear"""
 
 year = int(input())
-if year == "1500":
-    print("yes")
-if year > 1582:
-    if year % 4 == 0:
+if year < 1582:
+    if not year % 4:
         print("yes")
-    elif year % 4 != 0:
+    else:
         print("no")
-    elif year % 100 == 0:
+else:
+    if (not year % 400) or (not year % 4 and year % 100):
+        print("yes")
+    else:
         print("no")
-elif year < 1582:
-    print("no")
