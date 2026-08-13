c2423892fd164eddf7e464138adda6b888309cf7
Tun6970065IT <69070065@kmitl.ac.th>
2026-08-13 12:08:10 +0700

    Update 3101.py

--- 3101.py
+++ 3101.py
@@ -3,16 +3,16 @@
 temp = int(input())
 unit = input().lower()
 if unit == "c":
-    if not temp:
+    if  temp <= 0:
         print("solid")
-    elif temp == 100:
+    elif temp >= 100:
         print("gas")
     elif 0 < temp < 100:
         print("liquid")
 elif unit == "f":
-    if temp == 32:
+    if temp <= 32:
         print("solid")
-    elif temp == 212:
+    elif temp >= 212:
         print("gas")
     elif 32 < temp < 212:
         print("liquid")
