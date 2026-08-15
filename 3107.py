e22290021ac9e1110e8a739680097495c8e61919
Tun6970065IT <69070065@kmitl.ac.th>
2026-08-15 11:03:18 +0700

    Update 3107.py

--- 3107.py
+++ 3107.py
@@ -7,30 +7,30 @@ postion = po_split[0]
 age = int(po_split[1])
 salary = int(po_split[2])
 if postion == "M":
-    if age < 5:
+    if age <= 5:
         bonus = (6/100) * salary + 1500
         print(int(bonus))
-    elif 5 <= age <= 10:
+    elif 5 < age <= 10:
         bonus = (8/100) * salary + 1500
         print(int(bonus))
     elif age > 10:
         bonus = (10/100) * salary + 1500
         print(int(bonus))
 elif postion == "B":
-    if age < 5:
+    if age <= 5:
         bonus = (5/100) * salary + 1000
         print(int(bonus))
-    elif 5 <= age <= 10:
+    elif 5 < age <= 10:
         bonus = (6/100) * salary + 1000
         print(int(bonus))
     elif age > 10:
         bonus = (7/100) * salary + 1000
         print(int(bonus))
 elif postion == "G":
-    if age < 5:
+    if age <= 5:
         bonus = (4/100) * salary + 500
         print(int(bonus))
-    elif 5 <= age <= 10:
+    elif 5 < age <= 10:
         bonus = (5/100)*salary + 500
         print(int(bonus))
     elif age > 10:
