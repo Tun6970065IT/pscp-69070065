4cd36a67883ae6d7e2de1167ac330fe8998c55a2
Tun6970065IT <69070065@kmitl.ac.th>
2026-08-27 19:08:20 +0700

    Update 3112.py

--- 3112.py
+++ 3112.py
@@ -1,16 +1,47 @@
 """milkteabubble"""
+
 n = input()
-n_split = n.split(" ")
+n_split = n.split()
 energy_type = n_split[0]
-energy_percc = int(n_split[1])
+energy_percc = float(n_split[1])
 
 x = input()
-x_split = x.split(" ")
+x_split = x.split()
 bb_type = x_split[0]
 sweet = x_split[1]
-v = int(x_split[2])
+v = float(x_split[2])
 
+total = 0
+if energy_type == "H":
+    total = 5 * energy_percc
+elif energy_type == "O":
+    total = 3 * energy_percc
+elif energy_type == "J":
+    total = 2 * energy_percc
 
-type_h = 5 * energy_percc
-type_o = 3 * energy_percc
-type_j = 2 * energy_percc
+if bb_type == "T":
+    if sweet == "1":
+        total += 15 * v
+    elif sweet == "2":
+        total += 20 * v
+    elif sweet == "3":
+        total += 30 * v
+elif bb_type == "R":
+    if sweet == "1":
+        total += 12 * v
+    elif sweet == "2":
+        total += 18 * v
+    elif sweet == "3":
+        total += 25 * v
+elif bb_type == "M":
+    if sweet == "1":
+        total += 10 * v
+    elif sweet == "2":
+        total += 15 * v
+    elif sweet == "3":
+        total += 20 * v
+
+if total.is_integer():
+    print(int(total))
+else:
+    print(total)
