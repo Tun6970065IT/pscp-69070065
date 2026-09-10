b56666ffb92c1dad61653d94e07295c7401201cc
Tun6970065IT <69070065@kmitl.ac.th>
2026-09-10 20:28:31 +0700

    Create 3353.py

---
+++ 3353.py
@@ -0,0 +1,11 @@
+"""PickThemAgain"""
+
+n = input().split()
+have = False
+for _ in reversed(n):
+    num = int(_)
+    if not num % 3 or not num % 5:
+        print(_)
+    have = True
+if not have:
+    print("Nope")
