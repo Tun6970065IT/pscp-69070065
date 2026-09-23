1a6951b80a4de04181385133407d5a7aabe7db36
Tun6970065IT <69070065@kmitl.ac.th>
2026-09-23 21:20:25 +0700

    Update 3292.py

--- 3292.py
+++ 3292.py
@@ -1,8 +1,10 @@
 """ARROWaaa"""
 
-def arrow():
-    """ARROWaaa"""
-    num = int(input())
+tid = input().strip()
+n = int(input())
+
+def r_arrow(num):
+    """rightarrow"""
     for i in range(num , 0 ,-1):
         space = " " * ((num - i) * 2)
         star = "*" * i
@@ -11,12 +13,22 @@ def arrow():
         space = " " * ((num - i) * 2)
         star = "*" * i
         print(f"{space}{star}")
+
+def l_arrow(num):
+    """leftarrow"""
     for i in range(num , 0 ,-1):
-        space = " " * (i -1 )
+        space = " " * (i -1)
         star = "*" * i
         print(f"{space}{star}")
     for i in range(2 , num + 1):
         space = " " * (i - 1)
         star = "*" * i
         print(f"{space}{star}")
-arrow()
+for j , char in enumerate(tid):
+    if j > 0:
+        print()
+
+    if char == "R":
+        r_arrow(n)
+    elif char == "L":
+        l_arrow(n)
